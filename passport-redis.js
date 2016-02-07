e806fdda94f8de529060398d5bdddaa8d741fc25
Chaz Leong <cleong14@gmail.com>
2016-02-06 18:59:31 -1000

    set up passport, sessions, and body-parser

--- passport-redis.js
+++ passport-redis.js
@@ -1,10 +1,53 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var session = require('express-session');
+var config = require('./config');
 
 var app = express();
 var router = express.Router();
 
 app.set('views', 'views');
 app.set('view engine', 'jade');
+
+// 1. body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// 2. sessions
+app.use(session({ secret: 'secret cat' }));
+
+app.use(session(config.session));
+
+// 3. passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function (user, done) {
+  return done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  return done(null, user);
+});
+
+// passport.use(new LocalStrategy(
+//   function (name, done) {
+//     User.find({
+//       where: {
+//         name: name
+//       }
+//     })
+//     .then(function (user) {
+//       if (!name) {
+//         return done(null, false);
+//       }
+//       if (user.name === name) {
+//         return done(null, user);
+//       }
+//     });
+//   }  
+// ));
 
 router.route('/info')
   .get(function (req, res) {
